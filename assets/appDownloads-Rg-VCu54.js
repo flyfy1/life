1f0ae0521ac476a8f6c11d7e9998ea0163c5a737
Song Yangyu <flyfy1@gmail.com>
2026-05-30 15:56:09 +0800

    Updates

--- assets/appDownloads-Rg-VCu54.js
+++
@@ -1,1 +0,0 @@
-const t={mac:"https://files.integ.life/integ-mac@latest.zip",android:"https://files.integ.life/integ-android@latest.apk",wear:"https://files.integ.life/integ-wear@latest.apk"};export{t as A};
